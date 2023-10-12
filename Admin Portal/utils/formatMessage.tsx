95fe0883800b37c87040e64b1c7e0683176aedc2
Hasin <h.zaman.23010@khi.iba.edu.pk>
2023-10-12 19:54:30 +0500

    commit3

--- Admin Portal/utils/formatMessage.tsx
+++
@@ -1,9 +0,0 @@
-const formatMessage = (complaint: String) => {
-    const words = complaint.split(' ');
-    if (words.length > 3) {
-      return words.slice(0, 3).join(' ') + '...';
-    }
-    return complaint;
-};
-
-export default formatMessage;
